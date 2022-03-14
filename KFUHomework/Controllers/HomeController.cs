7208a1de5e159cd9e6d6e02b64938601042115e7
Bernad Jean <rasul.mingazov@gmail.com>
2022-03-14 22:13:05 +0300

    SecondTask

--- KFUHomework/Controllers/HomeController.cs
+++ KFUHomework/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace KFUHomework.Controllers
         {
             return View();
         }
+        
+        public IActionResult SecondTask()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
