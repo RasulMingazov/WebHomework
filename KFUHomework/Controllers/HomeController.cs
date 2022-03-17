63e444c34d149a6223f2d98221ace489395b616b
Bernad Jean <rasul.mingazov@gmail.com>
2022-03-17 05:33:15 +0300

    testing

--- KFUHomework/Controllers/HomeController.cs
+++ KFUHomework/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace KFUHomework.Controllers
         {
             return View();
         }
+        
+        public IActionResult CopySiteTask()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
